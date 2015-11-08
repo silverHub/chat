34d00e06b4bf348a1217f0cca4dfc755d101c990
Tamas Hoffman <tms.hoffman@gmail.com>
2015-11-08 16:59:37 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  io.emit('player connected', '');
+  console.log('a user connected');
+
+  socket.on('disconnect', function(){
+	io.emit('player disconnected', '');
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+	console.log('message: ' + msg);
+  });
+  socket.broadcast.emit('hi');
+});
+
+io.emit('some event', { for: 'everyone' });
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
