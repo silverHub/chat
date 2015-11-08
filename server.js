f7646cc2c41c5bad65d05fb1cce436c36ab7bdf0
Tamas Hoffman <tms.hoffman@gmail.com>
2015-11-08 18:10:28 +0100

    Nicks can be used. Displays when user enter/leave

--- server.js
+++ server.js
@@ -2,26 +2,45 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var users = [];
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  io.emit('player connected', '');
-  console.log('a user connected');
-
-  socket.on('disconnect', function(){
-	io.emit('player disconnected', '');
-    console.log('user disconnected');
-  });
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-	console.log('message: ' + msg);
-  });
-  socket.broadcast.emit('hi');
+app.get('/main.css', function(req, res){
+  res.sendFile(__dirname + '/main.css');
 });
 
-io.emit('some event', { for: 'everyone' });
+
+app.get('/nick/:nickname', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+io.on('connection', function(socket){
+
+  socket.on('connectionWithNick', function(nick){
+	   io.emit('chat message', nick+ ' just now connected...');
+     users.push(nick);
+  });
+
+  socket.on('disconnectionWithNick', function(nick){
+	   io.emit('chat message', nick+ ' has left the chat...');
+     delete users[nick];
+  });
+
+
+  socket.on('disconnect', function(){
+	   io.emit('player disconnected', '');
+
+  });
+
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+
+});
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
